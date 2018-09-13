558e7b17e01079f6e6d4e5820a13d38bf139e336
Mauricio Karchmer <mauricio.karchmer@gmail.com>
2018-09-13 15:50:03 -0400

    cleanned comments

--- js/form.js
+++ js/form.js
@@ -36,26 +36,6 @@ var RACheckArray = [
 	"RA_prop_att",
 ]; // 10
 
-/*
-var XTICheckDict = {
-
-	TI_refl_listen: "Reflective Listening",
-	TI_encour: "Encouragement",
-	TI_decis_balnc: "Decisional Balancing",
-	TI_prob_solv: "Problem Solving",
-	TI_pos_reinforce: "Positive Reinforcemnt",
-	TI_explore: "Exploring",
-	TI_play_therapy: "Play Therapy",
-	TI_valid: "Validation",
-	TI_conf_behav: "Confronting of Behaviors",
-	TI_real_test: "Reality Testing",
-	TI_PSCR: "Provide Safe Consistent Relationship",
-	TI_CSB: "Communication Skill Building",
-	TI_PSB: "Parenting Skill Building",
-	TI_emot_supp: "Emotional Supportiveness",
-};*/ // 14
-
-
 var TITextArray = [
 	"TI_other_txt",
 ]; // 1
@@ -79,82 +59,6 @@ var GTextArray = [
 	"G_2_decr_txt",
 	"G_other_txt",
 ]; // 5
-
-/*
-var XSPACheckDict = {
-
-	SPA_AX: "Anxiousness",
-	SPA_EW: "Excessive worry",
-	SPA_HV: "Hyper Vigilance",
-	SPA_DRT: "Disturbing rememberances of trauma",
-	SPA_SM: "Sad mood",
-	SPA_DM: "Depressed mood",
-	SPA_RRA: "Restricted range of affet",
-	
-	SPA_ML: "Mood liability",
-	SPA_MUT: "Mood upset / tearfulness",
-	SPA_S: "Stress",
-	SPA_LF: "Lonely feelings",
-	SPA_I: "Isolation",
-	SPA_LI: "Loss of interest",
-
-	SPA_FA: "Flat affect",
-	SPA_MS: "Monotone speech",
-	SPA_PS: "Preassured speech",
-	SPA_LE: "Low energy",
-	SPA_LM: "Low motivation",
-
-	SPA_ANG: "Anger",
-	SPA_IRR: "Irritability",
-	SPA_ARG: "Argumentative",
-	SPA_AGI: "Agitation",
-	SPA_FRU: "Frustration",
-
-	SPA_GF: "Guilt feelings",
-	SPA_OT: "Obsessive thinking",
-	SPA_PER: "Perseveration",
-	SPA_INT: "Intellectualization",
-
-	SPA_AHA: "Auditory hallucinations",
-	SPA_VHA: "Visual hallucinations",
-	SPA_DIT: "Disorganized thinking",
-
-	SPA_PAP: "Poor appetite",
-	SPA_PSL: "Poor sleep",
-	SPA_PSC: "Poor self care",
-	SPA_PHY: "Poor hygiene",
-
-	SPA_INTP: "Interpersonal problems",
-	SPA_FDP: "Family dynamic problems",
-	SPA_MARC: "Marital conflict",
-	SPA_SIDI: "Sibling discord",
-	SPA_PACH: "Parental challenges",
-
-	SPA_INTI: "Intimacy problems",
-	SPA_PSEC: "Poor self esteem / confidence",
-	SPA_HSC: "Harsh self criticism",
-
-	SPA_WPP: "Work performance problems",
-	SPA_WSTR: "Work stress",
-	SPA_SPP: "School performance problems",
-	SPA_SSTR: "School stress",
-
-	SPA_MOTREST: "Motor restlessness",
-	SPA_EABOR: "Easily bored",
-	SPA_DWF: "Difficulty with focus",
-	SPA_IMPU: "Impulsivity",
-	SPA_DIFTRAN: "Difficuly transitioning",
-	SPA_ACTOUT: "Acting out",
-
-	SPA_ALCHUSE: "Alcohol use",
-	SPA_DRGUSE: "Drug use",
-	SPA_RECREL: "Recent relapse",
-	SPA_URGSUSE: "Urges to use",
-
-	SPA_FINDIF: "Financial Difficulties",
-	SPA_MEDDIF: "Medical problems",
-	SPA_MEDNC: "Medication non-compliance",
-} */ // 59
 
 var SPATextArray = [
 	"SPA_other_txt",
@@ -186,8 +90,6 @@ var PLNTextArray = [
 	"PLN_NXT",
 	"PLN_other_txt",
 ]; // 4
-
-
 
 // utility functions to build the form
 
@@ -230,25 +132,6 @@ function addOtherToList(selection, name){
 		.attr('id',name)
 		.classed('form-control',true)
 }
-
-/*
-function createFormSection(id, data){
-	d3.select(id).select('ul')
-	  .selectAll('li')
-	  .data(Object.entries(data))
-	  .enter()
-	  .append('li')
-	  .classed('list-group-item',true)
-	  .append('input')
-	  .attr('type','checkbox')
-	  .attr('disabled',true)
-	  .attr('name', (d) => d[0]);
-	d3.select(id)
-	  .selectAll('li')
-	  .data(Object.entries(data))
-	  .append('span')
-	  .text((d) => (" " + d[1]));
-}*/
 
 function createFormSectionArr(id, data){
 	d3.select(id)//.select('ul')
